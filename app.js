53b8182b646bf1a467af14e415b0e77ecff6afd4
prestonh7 <prestonhouse7@gmail.com>
2023-02-24 14:57:21 -0600

    Books are added to dom

--- app.js
+++ app.js
@@ -16,18 +16,45 @@ function addBookToLibrary(book) {
 }
 
 function updateList() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i];
-        let bookElement = document.createElement("div");
-        bookElement.className = "book";
-        bookElement.innerText = 
-        `<h3>${book.title}</h3>
-        <p>Author: ${book.author}</p>
-        <p>Pages: ${book.pages}</p>
-        <p>Read: ${book.read}</p>`;
-        container[0].appendChild(bookElement);
-    }
-}
+    const bookList = document.getElementById("book-list");
+    bookList.innerText = ""; // clear the current list
+  
+    myLibrary.forEach((book, index) => {
+      const li = document.createElement("li");
+      const title = document.createElement("h3");
+      const author = document.createElement("p");
+      const pages = document.createElement("p");
+      const read = document.createElement("button");
+      const remove = document.createElement("button");
+  
+      // set the text content of the elements
+      title.textContent = book.title;
+      author.textContent = book.author;
+      pages.textContent = book.pages;
+      read.textContent = book.read ? "Read" : "Not read";
+      remove.textContent = "Remove";
+  
+      // add event listeners to the buttons
+      read.addEventListener("click", () => {
+        toggleRead(index);
+        updateList();
+      });
+      remove.addEventListener("click", () => {
+        removeBook(index);
+        updateList();
+      });
+  
+      // append the elements to the li element
+      li.appendChild(title);
+      li.appendChild(author);
+      li.appendChild(pages);
+      li.appendChild(read);
+      li.appendChild(remove);
+  
+      // append the li element to the ul element
+      bookList.appendChild(li);
+    });
+  }
 
 let berserk = new Book("Berserk", "Kentaro Miura", "~700", "yes");
 
